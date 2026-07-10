d7ac6ad7a937f13b53c9a5fa190295650f19abca
24124s1 <rpica640@gmail.com>
2026-07-10 04:42:58 -0400

    Update ui src.lua

--- ui src.lua
+++ ui src.lua
@@ -3165,18 +3165,26 @@
 				})
 			--  
 
-			function cfg.set(value)
-				if type(value) == "userdata" then 
-					return 
+			function cfg.set(value) 
+				local selected = {}
+				local is_table = type(value) == "table"
+				for _,v in next, cfg.option_instances do 
+					if v.Text == value or (is_table and find(value, v.Text)) then 
+						insert(selected, v.Text)
+						cfg.multi_items = selected
+						v.TextColor3 = themes.preset.accent
+					else 
+						v.TextColor3 = themes.preset.text
+					end
 				end
-
-				cfg.value = math.clamp(library:round(value, cfg.intervals), cfg.min, cfg.max)
-
-				fill.Size = dim2((cfg.value - cfg.min) / (cfg.max - cfg.min), 0, 1, 0)
-				slidertext.Text = tostring(cfg.value) .. cfg.suffix .. "/" .. tostring(cfg.max) .. cfg.suffix
-				flags[cfg.flag] = cfg.value
-
-				cfg.callback(flags[cfg.flag])
+				text.Text = is_table and concat(selected, ", ") or selected[1] or "nun"
+				flags[cfg.flag] = is_table and selected or selected[1]
+				if cfg.inline then
+					task.spawn(function()
+						dropdown_REAL.Size = dim2(0, text.TextBounds.X + 24, 0, 14)
+					end)
+				end
+				cfg.callback(flags[cfg.flag]) 
 			end
 
 			function cfg.set_element_visible(bool)
@@ -4439,21 +4447,21 @@
 		    }
 		    cfg.default = options.default or (cfg.multi and {cfg.items[1]}) or cfg.items[1] or nil
 			local dropdown_REAL = library:create("TextLabel", {
-    			Parent = parent,
-    			FontFace = library.font,
-    			TextColor3 = themes.preset.text,
-    			BorderColor3 = rgb(0, 0, 0),
-    			Text = "",
-    			Name = "dropdown",
-    			ZIndex = 2,
-    			Size = cfg.inline and dim2(0, 45, 0, 14) or dim2(1, -8, 0, 12),
-    			BorderSizePixel = 0,
-    			BackgroundTransparency = 1,
-    			TextXAlignment = Enum.TextXAlignment.Left,
-   		 		AutomaticSize = Enum.AutomaticSize.Y,
-    			TextYAlignment = Enum.TextYAlignment.Top,
-    			TextSize = 12,
-    			BackgroundColor3 = rgb(255, 255, 255)
+				Parent = parent,
+				FontFace = library.font,
+				TextColor3 = themes.preset.text,
+				BorderColor3 = rgb(0, 0, 0),
+				Text = "",
+				Name = "dropdown",
+				ZIndex = 2,
+				Size = cfg.inline and dim2(0, 45, 0, 14) or dim2(1, -8, 0, 12),
+				BorderSizePixel = 0,
+				BackgroundTransparency = 1,
+				TextXAlignment = Enum.TextXAlignment.Left,
+				AutomaticSize = Enum.AutomaticSize.Y,
+				TextYAlignment = Enum.TextYAlignment.Top,
+				TextSize = 12,
+				BackgroundColor3 = rgb(255, 255, 255)
 			})
 		    if cfg.inline then
 		        dropdown_REAL.LayoutOrder = -1
@@ -4551,18 +4559,18 @@
 		        if main_text then main_text.Visible = bool end
 		    end 
 			function cfg.set_visible(bool) 
-    			library.current_element_open = cfg.ignore or cfg
-    			dropdown_holder.Visible = bool
-    			plus.Text = bool and "-" or "+"
-    			plus.TextSize = bool and 12 or 8
-    			if bool then 
-        			if library.current_element_open and library.current_element_open ~= cfg and not cfg.ignore then 
-            			library.current_element_open.set_visible(false)
-            			library.current_element_open.open = false 
-        			end
-        			dropdown_holder.Size = dim2(0, math.max(dropdown.AbsoluteSize.X, 60), 0, dropdown_holder.Size.Y.Offset)
-        			dropdown_holder.Position = dim2(0, dropdown.AbsolutePosition.X + 1, 0, dropdown.AbsolutePosition.Y + 22)                    
-   				end
+				library.current_element_open = cfg.ignore or cfg
+				dropdown_holder.Visible = bool
+				plus.Text = bool and "-" or "+"
+				plus.TextSize = bool and 12 or 8
+				if bool then 
+					if library.current_element_open and library.current_element_open ~= cfg and not cfg.ignore then 
+						library.current_element_open.set_visible(false)
+						library.current_element_open.open = false 
+					end
+					dropdown_holder.Size = dim2(0, math.max(dropdown.AbsoluteSize.X, 60), 0, dropdown_holder.Size.Y.Offset)
+					dropdown_holder.Position = dim2(0, dropdown.AbsolutePosition.X + 1, 0, dropdown.AbsolutePosition.Y + 22)                    
+				end
 			end
 		    function cfg.set(value) 
 		        local selected = {}
