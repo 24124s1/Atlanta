86c86b76fc036085d1b4d6038f498d4d4a1946c2
090 <rpica640@gmail.com>
2026-07-31 16:40:58 -0400

    Enable highlights for entities not in raid

--- EspLibrary.lua
+++ EspLibrary.lua
@@ -278,7 +278,7 @@ local function ProcessEntity(entity)
         if not inRaid or not inRaid:IsA("BoolValue") or not inRaid.Value then
             if espModule._drawings[entity] then
                 HideESP(espModule._drawings[entity])
-                if espModule._highlights[entity] then espModule._highlights[entity].Enabled = false end
+                if espModule._highlights[entity] then espModule._highlights[entity].Enabled = true end
             end
             return
         end
