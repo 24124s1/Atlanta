1ef24b3e5b308f4c8f44dd65dd94e5124d7184b3
090 <rpica640@gmail.com>
2026-08-04 22:12:58 -0400

    Update ui src.lua

--- ui src.lua
+++ ui src.lua
@@ -5583,7 +5583,7 @@
 	local sound_service = cloneref(game:GetService("SoundService"))
 	local starter_gui = cloneref(game:GetService("StarterGui"))
 	local rs = cloneref(game:GetService("ReplicatedStorage"))
-	local MarketplaceService = cloneref(game:GetService("MarketplaceService")
+	local MarketplaceService = cloneref(game:GetService("MarketplaceService"))
 
 	local vec2 = Vector2.new
 	local vec3 = Vector3.new
@@ -7186,6 +7186,22 @@
 						watermark.change_text(os.date('Vagrant.cc - Beta - %b %d %Y - %H:%M:%S'))
 					end 
 				end) 
+
+				local gameName = "Unknown Game"
+				pcall(function()
+    				gameName = Services.MarketplaceService:GetProductInfo(game.PlaceId).Name
+				end)
+
+
+				local watermark = Library:watermark({
+    				default = string.format("catware.top | private | %s | %s", gameName)
+				})  
+
+				task.spawn(function()
+    				while task.wait(1) do 
+        				watermark.change_text(string.format("catware.top | private | %s | %s", gameName))
+    				end 
+				end)
 
 				local items = style.items
 
