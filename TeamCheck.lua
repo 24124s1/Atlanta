d5efc8582c46dd4c20ce810c42a081857507e8d2
090 <rpica640@gmail.com>
2026-08-04 21:46:45 -0400

    Update TeamCheck.lua

--- TeamCheck.lua
+++ TeamCheck.lua
@@ -1,7 +1,9 @@
 getgenv().cloneref = cloneref or function(...) return ... end
 local ReplicatedStorage = cloneref(game:GetService("ReplicatedStorage"))
 local Players = cloneref(game:GetService("Players"))
-local LocalPlayer = Players.LocalPlayer
+local Services = {
+    LocalPlayer = Players.LocalPlayer
+}
 
 local TeamManager = {
     Members = {},
@@ -10,14 +12,29 @@ local TeamManager = {
 if game.PlaceId == 115872975504419 then
     local SquadRemotes = ReplicatedStorage:WaitForChild("SquadRemotes")
     SquadRemotes.SquadUpdate.OnClientEvent:Connect(function(data)
-		if data and data.members then TeamManager.Members = data.members end
-	end)
+        if data and data.members then 
+            TeamManager.Members = data.members 
+        end
+    end)
 end
 
-local IsTeam = function (plr)
-    if game.PlaceId == 112757576021097 then return LocalPlayer.PlayerStates.Team.Value == plr.PlayerStates.Team.Value end
-    if game.PlaceId == 115872975504419 then return table.find(TeamManager.Members, plr.UserId) end
-    return plr.Team == LocalPlayer.Team
-end
+local function isTeammate(player)
+    if not player or player == Services.LocalPlayer then return false end
 
-return IsTeam
+    if game.PlaceId == 112757576021097 then
+        if Services.LocalPlayer:FindFirstChild("PlayerStates") and player:FindFirstChild("PlayerStates") then
+            return Services.LocalPlayer.PlayerStates.Team.Value == player.PlayerStates.Team.Value
+        end
+    end
+
+    if game.PlaceId == 115872975504419 then
+        if table.find(TeamManager.Members, player.UserId) then return true end
+    end
+
+    if player.Team and player.Team == Services.LocalPlayer.Team then return true end
+
+    local character = player.Character
+    if character and character:FindFirstChild("SquadBillboard") then return true end
+
+    return false
+end
