6700083aed965ad22592a789319e1c81b20e5b65
090 <rpica640@gmail.com>
2026-08-04 22:24:27 -0400

    Simplify watermark text formatting
    Refactor watermark creation to simplify game name display.

--- ui src.lua
+++ ui src.lua
@@ -7180,17 +7180,14 @@
 
 				local gameName = "Unknown Game"
 				pcall(function()
-    				gameName = Services.MarketplaceService:GetProductInfo(game.PlaceId).Name
+    				gameName = MarketplaceService:GetProductInfo(game.PlaceId).Name
 				end)
 
-
-				local watermark = library:watermark({
-    				default = string.format("catware.top | private | %s | %s", gameName)
-				})  
+				local watermark = library:watermark({default = string.format("catware.top | private | %s", gameName)})  
 
 				task.spawn(function()
     				while task.wait(1) do 
-        				watermark.change_text(string.format("catware.top | private | %s | %s", gameName))
+        				watermark.change_text(string.format("catware.top | private | %s", gameName))
     				end 
 				end)
 
